8ad286fd33a46907535b0273a1110a2c549f65ea
Chamikara <pramudithachamikara@gmail.com>
2024-07-29 10:13:15 +0530

    Remove unnecessary comments

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 ## Test Assignments given by Taskina
 
 # Assignment 1
+
+```
+/
+```
+
+# Assignment 2
+
+```
+/assignment-2
+/assignment-2/login
+/assignment-2/register
+```
--- auth.ts
+++ auth.ts
@@ -1,4 +1,4 @@
-import NextAuth, { DefaultSession, User } from "next-auth";
+import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
 import { getUser } from "./src/actions/api";
@@ -16,32 +16,21 @@ export const { auth, signIn, signOut, handlers } = NextAuth({
         password: {},
       },
       authorize: async (credentials) => {
-        // console.log("hits...");
-        // console.log("credentials", credentials);
-
         const parsedCredentials = z
           .object({ username: z.string().min(4), password: z.string().min(6) })
           .safeParse(credentials);
 
         if (parsedCredentials.success) {
           const { username, password } = parsedCredentials.data;
-
           const user = await getUser(username);
-
-          // console.log("user", user);
 
           if (!user) throw new Error("User not found.");
 
           const passwordsMatch = await bcrypt.compare(password, user.password);
-
-          // console.log("passwordsMatch", passwordsMatch);
-
           const newU = { ...user, name: user.username, email: user.user_id.toString() };
 
           if (passwordsMatch) return newU;
         }
-
-        // console.log("Invalid credentials");
 
         return null;
       },
